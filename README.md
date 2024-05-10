90ca768d6682c070c96684f2b42efef84dd3aaad
kaeini <spiderdan19@gmail.com>
2024-05-10 18:06:56 +0100

    Added the ability to playback videos

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # Deltafin - A Jellyfin CLI Client.
 
-This doesn't do much for now. Here are the following features:
+A very basic CLI client for Jellyfin written in Python to playback videos.
+
+Here are the implemented features:
 
 - Can log in to a server (through HTTP or reverse proxy).
 - Ability to show publicly available users
 - Ability to save user credentials.
-- Shows user's categories and media from that category.
+- Shows user's categories and media from the selected category.
+- Playback selected content
 
 
-Planned:
-- Be able to select content from library.
-- Ability to select and playback content (probably through MPV).
+Planned features:
+
 - Ability to login through API Key
+- Handle navigation better for media with subfolders (i.e. Music/TV Shows)
+- Grab the specific ID of a selected category/media file without user interaction.
 
 
 Please note, this isn't intended to be a fully fledged client. I'm mainly using this to practice web scraping and using APIs.
--- library.py
+++ library.py
@@ -1,4 +1,6 @@
 import json
+import mpv
+
 
 def library_func(client):
     try:
@@ -18,11 +20,29 @@ def library_func(client):
                 # TODO: Find an alternative way of scraping TV shows and music categories.
                 category_id = input('Enter the category ID: ')
                 try:
-                    #Searches through the library for that category ID and outputs it.
+                    # Searches through the library for that category ID and outputs it.
                     category = client.jellyfin.user_items(params={'Recursive': True, 'ParentId': category_id})
                     print('Media in the category:')
                     for video in category.get('Items', []):
                         print(video['Name'])
+                        print(video['Id'])
+                    choice = input("Do you want to play a video (y/n): ")
+                    if choice == "y":
+                        video_id = input('Enter the video ID: ')
+                        try:
+                            # Plays the video in mpv
+                            player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True)
+                            player.register_key_binding("CLOSE_WIN","quit")
+                            player.fullscreen = True
+                            player.play(client.jellyfin.video_url(video_id))
+
+                            # Handles cleanly closing the video
+                            player.wait_for_playback()
+                            player.terminate()
+                        except:
+                            print("Invalid video ID!")
+                    else:
+                        print('Exiting...')
                 # TODO: Make the exception handling more specific.
                 except:
                     print('ERROR: Invalid category ID!')
