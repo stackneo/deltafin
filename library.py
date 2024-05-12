3feb59b253c8dbe17db4626ba6f3c14c0ed235fe
kaeini <spiderdan19@gmail.com>
2024-05-12 23:03:18 +0100

    Reworked library navigation, IDs automatically grabbed

--- library.py
+++ library.py
@@ -20,65 +20,70 @@ def library_func(client):
                     if folder['Name'].lower() == category:
                         category_id = folder['Id']
                 try:
+                    # Dict to store media ID's for playback
+                    media_dict = {}
                     # Grabs the relevant category ID for the user's choice
                     if category == "movies":
                         category = client.jellyfin.user_items(params={'Recursive': True, 'ParentId': category_id})
                         print('Movies:')
                         # Prints the movies in the category
-                        for video in category.get('Items', []):
-                            print(video['Name'])
-                            print(video['Id'])
+                        for i, video in enumerate(category.get('Items', []), start=1):
+                            print(f"{i}. {video['Name']}")
+                            media_dict[i] = video['Id']
                         playback = input("Do you want to play a video (y/n): ")
-
 
                     elif category == "shows":
                         # Shows are handled differently due to season/episode structure.
                         # The user must select a show, then a season, then an episode.
                         category = client.jellyfin.user_items(params={'Recursive': False, 'ParentId': category_id})
                         print('TV Shows:')
-                        for show in category.get('Items', []):
-                            print(show['Name'])
-                            print(show['Id'])
+                        for i, show in enumerate(category.get('Items', []), start=1):
+                            print(f"{i}. {show['Name']}")
+                            media_dict[i] = show['Id']
                         choice = input("Do you want to select a show (y/n): ")
                         if choice == "y":
-                            show_id = input('Enter the show ID: ')
+                            show_num = int(input('Enter show number: '))
+                            show_id = media_dict.get(show_num)
                             show = client.jellyfin.user_items(params={'Recursive': False, 'ParentId': show_id})
                             print('Seasons:')
-                            for season in show.get('Items', []):
-                                print(season['Name'])
-                                print(season['Id'])
+                            for i, season in enumerate(show.get('Items', []), start=1):
+                                print(f"{i}. {season['Name']}")
+                                media_dict[i] = season['Id']
                             season_choice = input("Do you want to select a season (y/n): ")
                             if season_choice == "y":
-                                season_id = input('Enter the season ID: ')
+                                season_num = int(input('Enter season number: '))
+                                season_id = media_dict.get(season_num)
                                 season = client.jellyfin.user_items(params={'Recursive': False, 'ParentId': season_id})
                                 print('Episodes:')
-                                for episode in season.get('Items', []):
-                                    print(episode['Name'])
-                                    print(episode['Id'])
+                                for i, episode in enumerate(season.get('Items', []), start=1):
+                                    print(f"{i}. {episode['Name']}")
+                                    media_dict[i] = episode['Id']
                                 playback = input("Do you want to play an episode (y/n): ")
 
                     elif category == "music":
                         # Music is handled differently due to album/song structure.
                         # The user must select an album then song to play.
 
-                        #TODO: Figure out why MPV displays nothing when playing music.
+                        # TODO: Figure out why MPV displays nothing when playing music.
                         category = client.jellyfin.user_items(params={'Recursive': False, 'ParentId': category_id})
                         print('Music: ')
-                        for song in category.get('Items', []):
-                            print(song['Name'])
-                            print(song['Id'])
+                        for i, song in enumerate(category.get('Items', []), start=1):
+                            print(f"{i}. {song['Name']}")
+                            media_dict[i] = song['Id']
                         choice = input("Do you want to select an album (y/n): ")
                         if choice == "y":
-                            album_id = input('Enter the album ID: ')
+                            album_num = int(input('Enter the album number: '))
+                            album_id = media_dict.get(album_num)
                             album = client.jellyfin.user_items(params={'Recursive': False, 'ParentId': album_id})
                             print('Songs:')
-                            for song in album.get('Items', []):
-                                print(song['Name'])
-                                print(song['Id'])
+                            for i, song in enumerate(album.get('Items', []), start=1):
+                                print(f"{i}. {song['Name']}")
+                                media_dict[i] = song['Id']
                             playback = input("Do you want to play a song (y/n): ")
                     if playback == "y":
                         # User must input the media ID to play the video.
-                        video_id = input('Enter the media ID: ')
+                        video_num = int(input('Enter the media number: '))
+                        video_id = media_dict.get(video_num)
                         # Plays the video in mpv
                         player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True)
                         player.register_key_binding("CLOSE_WIN", "quit")
@@ -90,9 +95,8 @@ def library_func(client):
                         player.terminate()
                     else:
                         print('Exiting...')
-                # TODO: Make the exception handling more specific.
                 except:
-                    print('ERROR: Invalid category!')
+                    print('ERROR: Invalid input!')
 
     # If credentials.json not found, prompt the user to login.
     except FileNotFoundError:
